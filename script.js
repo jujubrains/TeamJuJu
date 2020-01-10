fd9a6d0cd0ba53bfcfd9a14f2ee6cf05b0e7c093
Alex Urbini <aurbini92@gmail.com>
2020-01-10 11:28:55 -0500

    adding psydocode and starter code

---
+++ script.js
@@ -0,0 +1,35 @@
+var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var todoList = document.querySelector("#todo-list");
+var todoCountSpan = document.querySelector("#todo-count");
+
+var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+
+
+//Starter Game Render ToDo
+//empty array 
+
+
+//Create new to do element 
+//Listen for the submit
+// Make toDo Element
+//add text to the toDo element
+//add it to the to do data
+//append/re-display it to the page
+
+
+
+
+//Remove to do elements
+
+
+
+
+//counter function 
+
+
+
+
+
+
